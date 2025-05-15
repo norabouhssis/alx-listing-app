41c0cac6542f1e2e638e85c0cd59e2509ba71bdd
Noura Bouhssis <nora.bouhssis@gmail.com>
2025-05-15 17:42:52 +0100

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -1,1 +1,1 @@
-Define all TypeScript interfaces related to the project. Start by creating placeholder interfaces for CardProps and ButtonProps.
+Set up constants to store any reusable data or strings such as API URLs, configuration settings, or UI text
